8bc5f8c1a52b02f79b1809375dfdff9ef6b5a5bf
NicolasDefelippo <nicolasdefelippo@gmail.com>
2023-08-10 11:44:46 -0300

    Actualizo contador carrito

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 console.table(productos);
 
-// Función para filtrar productos por tipo
+// Filtrar productos por tipo
 function filtrarPorTipo(tipo) {
     const productosFiltrados = productos.filter(producto => producto.tipo === tipo);
     renderizarProductos(productosFiltrados);
 }
 
-// Inicializar mostrando todos los productos
+// HTML mostrando todos los productos
 document.addEventListener("DOMContentLoaded", () => {
     renderizarProductos(productos);
 });
 
-// Manejar clic en botones de filtrado por tipo
+// Botonera
 document.getElementById("pokemonBtn").addEventListener("click", () => filtrarPorTipo("pokemon"));
 document.getElementById("nickelodeonBtn").addEventListener("click", () => filtrarPorTipo("nickelodeon"));
 document.getElementById("animeBtn").addEventListener("click", () => filtrarPorTipo("anime"));
 document.getElementById("disneyBtn").addEventListener("click", () => filtrarPorTipo("disney"));
 document.getElementById("showAllBtn").addEventListener("click", () => {renderizarProductos(productos);});
 
-// Función para agregar producto al carrito
+// Agregar producto al carrito
 function agregarAlCarrito(producto) {
     const cartItem = document.createElement("li");
     cartItem.textContent = producto.nombre + " - $" + producto.precio;
     document.getElementById("cartItems").appendChild(cartItem);
 }
 
-// Manejar clic en botones "Agregar al Carrito"
+// Botones "Agregar al Carrito"
 document.getElementById("jscontainer").addEventListener("click", event => {
     if (event.target.classList.contains("add-to-cart")) {
         const productId = parseInt(event.target.getAttribute("data-id"));
@@ -34,7 +34,16 @@ document.getElementById("jscontainer").addEventListener("click", event => {
             agregarAlCarrito(product);
         }
     }
+    // Contador de productos en el carrito
+    actualizarContadorCarrito();
 });
+
+// Contador de productos en el carrito
+function actualizarContadorCarrito() {
+    const contador = document.getElementById("cartCounter");
+    const cantidadProductos = document.querySelectorAll("#cartItems li").length;
+    contador.textContent = cantidadProductos;
+}
 
 //DOM
 function renderizarProductos(listaProds){
